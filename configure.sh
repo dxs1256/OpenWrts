3a131413fcb969841c5266cb82ae699a33d17add
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2024-10-04 00:37:28 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Modify default system settings
 
-# 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
-
 # passwall
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
